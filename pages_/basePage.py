b2e2f1fe78847f06cb49fdc2b53594e00ae9f667
Nelly <nellikoko91@gmail.com>
2023-12-07 00:40:48 +0400

    QAG2-34 Create Base Page

---
+++ pages_/basePage.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from common_.utilities_.customLogger import *
+
+
+class BasePage():
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+
+    def _find_element(self, locator):
+        try:
+            element = WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located(locator))
+            return element
+        except:
+            logger("ERROR", "Element not found or wrong")
+            exit(1)
+
+    def _is_element_visible(self, locator):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            return True
+        except:
+            return False
+
+    def _elementy_should_be_visible(self, locator):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        except:
+            logger("ERROR", "Element should be visible but not")
+            exit(3)
+
+    def _click(self, webElement):
+        webElement.click()
+        logger("INFO", "The element successfully clicked")
+
+    def _fill_field(self, webElement, text):
+        webElement.clear()
+        webElement.send_keys(text)
+        logger("INFO", "Text successfully added to element")
+
+    def _get_title(self):
+        logger("INFO", "The title successfully found")
+        return self.driver.title
+
+    def _get_element_text(self, webElement):
+        logger("INFO", "Text successfully found")
+        return webElement.text
+
+    def _press_and_hold(self):
+        pass
+
+    def _get_element_text_by_locator(self, locator):
+        element = self._find_element(locator)
+        return element.text
+
+    def _double_click(self):
+        pass
+
+    def _mouse_move(self, element):
+        action = ActionChains(self.driver)
+        action.move_to_element(element)
+        action.perform()
+        logger("INFO", "Mouse successfully moved to element")
